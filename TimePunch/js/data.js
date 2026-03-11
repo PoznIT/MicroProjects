8149d4cbf014c6fae7e43a7d6e94f7fb9f641180
pozniako16 <lev.pozniakoff@gmail.com>
2026-03-11 11:29:00 +0100

    removed base punches

--- TimePunch/js/data.js
+++ TimePunch/js/data.js
@@ -1,14 +1,4 @@
-let days = JSON.parse(localStorage.getItem('timepunch_days') || 'null') || [
-  { date: '2026-03-02', punches: ['09:30', '12:10', '13:00', '16:22', '17:05', '18:30'] },
-  { date: '2026-03-03', punches: ['08:45', '12:10', '12:45', '17:30'] },
-  { date: '2026-03-04', punches: ['06:30', '08:00', '09:00', '12:00', '12:50', '17:00'] },
-  { date: '2026-03-05', punches: ['08:00', '12:00', '12:30', '16:30'] },
-  { date: '2026-03-06', punches: ['08:00', '12:00', '12:45', '17:30'] },
-  { date: '2026-03-07', punches: [] },
-  { date: '2026-03-08', punches: [] },
-  { date: '2026-03-09', punches: ['08:00', '12:00', '12:30', '17:00'] },
-  { date: '2026-03-10', punches: ['06:30', '08:25'] },
-];
+let days = JSON.parse(localStorage.getItem('timepunch_days') || "null") || [];
 
 function save() {
   localStorage.setItem('timepunch_days', JSON.stringify(days));
